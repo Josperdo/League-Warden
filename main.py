189c45f53a192d9930385f0c2ed247fbe902c211
John Sperry <john.sperry1@gmail.com>
2022-06-22 23:52:48 -0700

    Added quote feature

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import discord
+import os
+from dotenv import load_dotenv
+import requests
+import json
+
+# May need better way to store + access bot token long term
+load_dotenv()
 
 # reference discord.py library for function names
 
 client = discord.Client()
 
+# Initial Quote buildout
+
+
+def get_quote():
+    response = requests.get("https:://zenquotes.io/api/random")
+    json_data = json.leads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
+
+
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send('Hello!')
 
 # Runs bot
-client.run()
+client.run(os.getenv("DISCORD_TOKEN"))
