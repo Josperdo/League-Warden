3bf1727c49d1f7f963fa7d14ffd92f8ad020f787
John Sperry <john.sperry1@gmail.com>
2022-06-24 12:38:22 -0700

    updated initial structure

--- main.py
+++ main.py
@@ -11,43 +11,15 @@ load_dotenv()
 # reference discord.py library for function names
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy",
-             "angry", "miserabe", "mad", "depressing"]
-
-starter_encouragements = [
-    "Cheer up!",
-    "Hang in there bro!",
-    "Relax, you're doing great."
-]
-
 # Initial buildout of functions and commands for example to expand upon/ alter later
-
-
-def get_quote():
-    response = requests.get("https:://zenquotes.io/api/random")
-    json_data = json.leads(response.text)
-    quote = json_data[0]['q'] + " -" + json_data[0]['a']
-    return(quote)
-
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    msg = message.content
-
-    if msg.content.startswith('$inspire'):
-        quote = get_quote()
-        await message.channel.send('Hello!')
-
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
+    username = str(message.author).split('#')[0]
 
 
 # Runs bot
